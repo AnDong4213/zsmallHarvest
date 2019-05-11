834dedbce5a5f60ca003befb4f5c32fe053ad670
赵安东 <zhaoandong4213@sina.com>
2019-05-11 18:18:33 +0800

    05/11

--- 小程序/crf/crf.js
+++ 小程序/crf/crf.js
@@ -17,12 +17,7 @@ Page({
     console.log(this);
   }, */
 
-  navbarTap(e) {
-    let currentTab = e.currentTarget.dataset.index;
-    this.setData({
-      currentTab
-    })
-  },
+  
   change() {
     this.setData({
       'item.name': '呵呵呵呵'
@@ -36,40 +31,5 @@ Page({
   },
   pageEventListener(e) {
     console.log(e)
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
